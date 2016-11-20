e9237ffc1109cb33bc56ece877703e2bec8d41f3
DarkPyves <deeprevenge1@gmail.com>
2016-11-20 11:35:38 +0000

    Increased version number to 1.3

--- src/main/java/com/hm/petmaster/PetMaster.java
+++ src/main/java/com/hm/petmaster/PetMaster.java
@@ -42,7 +42,7 @@ import net.milkbowl.vault.economy.Economy;
  * Spigot project page: spigotmc.org/resources/pet-master.15904
  * 
  * @since December 2015.
- * @version 1.2.2
+ * @version 1.3
  * @author DarkPyves
  */
 
