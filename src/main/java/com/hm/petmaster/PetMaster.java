518f4200d43b3927724a5426885d44163b4faf7b
PyvesB <PyvesDev@gmail.com>
2017-12-21 20:42:57 +0100

    Updated to latest version of MCShared

--- src/main/java/com/hm/petmaster/PetMaster.java
+++ src/main/java/com/hm/petmaster/PetMaster.java
@@ -97,9 +97,8 @@ public class PetMaster extends JavaPlugin implements Listener {
 		// Check for available plugin update.
 		if (config.getBoolean("checkForUpdate", true)) {
 			updateChecker = new UpdateChecker(this, "https://raw.githubusercontent.com/PyvesB/PetMaster/master/pom.xml",
-					new String[] { "dev.bukkit.org/bukkit-plugins/pet-master/files",
-							"spigotmc.org/resources/pet-master.15904" },
-					"petmaster.admin", chatHeader);
+					"petmaster.admin", chatHeader, "spigotmc.org/resources/pet-master.15904",
+					"dev.bukkit.org/bukkit-plugins/pet-master/files");
 			pm.registerEvents(updateChecker, this);
 			updateChecker.launchUpdateCheckerTask();
 		}
