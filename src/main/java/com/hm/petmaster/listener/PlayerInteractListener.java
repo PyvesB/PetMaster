972aa064c028a8ff6e0079e2651c171719ecd4c8
PyvesB <PyvesDev@gmail.com>
2019-03-12 21:42:55 +0000

    Stopped displaying owner if name is null, refactored code

--- src/main/java/com/hm/petmaster/listener/PlayerInteractListener.java
+++ src/main/java/com/hm/petmaster/listener/PlayerInteractListener.java
@@ -6,7 +6,6 @@ import org.bukkit.Location;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Animals;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Llama;
 import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Parrot;
@@ -106,61 +105,70 @@ public class PlayerInteractListener implements Listener {
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event) {
-		// On Minecraft versions from 1.9 onwards, this event is fired twice, one for each hand. Need additional check.
-		if (version >= 9 && event.getHand() != EquipmentSlot.HAND) {
-			return;
-		}
+		if (shouldHandleEvent(event)) {
+			Tameable tameable = (Tameable) event.getRightClicked();
+			AnimalTamer currentOwner = tameable.getOwner();
+			if (currentOwner == null || currentOwner.getName() == null) {
+				return;
+			}
+			// Has the player clicked on one of his own pets?
+			Player player = event.getPlayer();
+			boolean isOwner = player.getName().equals(currentOwner.getName());
+			// Retrieve new owner from the map and delete corresponding entry.
+			Player newOwner = plugin.getSetOwnerCommand().collectPendingSetOwnershipRequest(player);
+			// Has the player requested to free one of his pets?
+			boolean freePet = plugin.getFreeCommand().collectPendingFreeRequest(player);
 
-		if (!(event.getRightClicked() instanceof Tameable) || ((Tameable) event.getRightClicked()).getOwner() == null
-				|| plugin.getEnableDisableCommand().isDisabled()) {
-			return;
-		}
+			// Cannot change ownership or free pet if not owner and no bypass permission.
+			if ((newOwner != null || freePet) && !isOwner && !player.hasPermission("petmaster.admin")) {
+				player.sendMessage(plugin.getChatHeader() + plugin.getPluginLang()
+						.getString("not-owner", "You do not own this pet!").replace("PLAYER", player.getName()));
+				return;
+			}
 
-		AnimalTamer currentOwner = ((Tameable) event.getRightClicked()).getOwner();
-		// Has the player clicked on one of his own pets?
-		boolean isOwner = event.getPlayer().getName().equals(currentOwner.getName());
-		// Retrieve new owner from the map and delete corresponding entry.
-		Player newOwner = plugin.getSetOwnerCommand().collectPendingSetOwnershipRequest(event.getPlayer());
-		// Has the player requested to free one of his pets?
-		boolean freePet = plugin.getFreeCommand().collectPendingFreeRequest(event.getPlayer());
-
-		// Cannot change ownership or free pet if not owner and no bypass permission.
-		if ((newOwner != null || freePet) && !isOwner && !event.getPlayer().hasPermission("petmaster.admin")) {
-			event.getPlayer().sendMessage(plugin.getChatHeader() + plugin.getPluginLang()
-					.getString("not-owner", "You do not own this pet!").replace("PLAYER", event.getPlayer().getName()));
-			return;
+			if (newOwner != null) {
+				changeOwner(player, currentOwner, newOwner, tameable);
+			} else if (freePet) {
+				freePet(player, currentOwner, tameable);
+			} else if ((displayToOwner || !isOwner) && player.hasPermission("petmaster.showowner")) {
+				displayHologramAndMessage(player, currentOwner, tameable);
+			}
 		}
+	}
 
-		if (newOwner != null) {
-			changeOwner(event, currentOwner, newOwner);
-		} else if (freePet) {
-			freePet(event, currentOwner);
-		} else if ((displayToOwner || !isOwner) && event.getPlayer().hasPermission("petmaster.showowner")) {
-			displayHologramAndMessage(event, currentOwner);
-		}
+	/**
+	 * Determines whether the current PlayerInteractEntityEvent should be handled.
+	 * 
+	 * @param event
+	 * @return true if the event should be handled, false otherwise
+	 */
+	private boolean shouldHandleEvent(PlayerInteractEntityEvent event) {
+		return !plugin.getEnableDisableCommand().isDisabled() && event.getRightClicked() instanceof Tameable
+				// On Minecraft 1.9+, the event is fired once per hand (HAND and OFF_HAND).
+				&& (version < 9 || event.getHand() == EquipmentSlot.HAND);
 	}
 
 	/**
 	 * Change the owner of a pet.
 	 * 
-	 * @param event
+	 * @param player
 	 * @param oldOwner
 	 * @param newOwner
+	 * @param tameable
 	 */
-	private void changeOwner(PlayerInteractEntityEvent event, AnimalTamer oldOwner, Player newOwner) {
-		if (chargePrice(event.getPlayer(), changeOwnerPrice)) {
-			Tameable tameableAnimal = (Tameable) event.getRightClicked();
+	private void changeOwner(Player player, AnimalTamer oldOwner, Player newOwner, Tameable tameable) {
+		if (chargePrice(player, changeOwnerPrice)) {
 			// Change owner.
-			tameableAnimal.setOwner(newOwner);
-			event.getPlayer().sendMessage(plugin.getChatHeader()
+			tameable.setOwner(newOwner);
+			player.sendMessage(plugin.getChatHeader()
 					+ plugin.getPluginLang().getString("owner-changed", "This pet was given to a new owner!"));
 			newOwner.sendMessage(plugin.getChatHeader()
 					+ plugin.getPluginLang().getString("new-owner", "Player PLAYER gave you ownership of a pet!")
-							.replace("PLAYER", event.getPlayer().getName()));
+							.replace("PLAYER", player.getName()));
 
 			// Create new event to allow other plugins to be aware of the ownership change.
 			PlayerChangeAnimalOwnershipEvent playerChangeAnimalOwnershipEvent = new PlayerChangeAnimalOwnershipEvent(
-					oldOwner, newOwner, tameableAnimal);
+					oldOwner, newOwner, tameable);
 			Bukkit.getServer().getPluginManager().callEvent(playerChangeAnimalOwnershipEvent);
 		}
 	}
@@ -168,29 +176,29 @@ public class PlayerInteractListener implements Listener {
 	/**
 	 * Frees a pet; it will no longer be tamed.
 	 * 
-	 * @param event
+	 * @param player
 	 * @param oldOwner
+	 * @param tameable
 	 */
-	private void freePet(PlayerInteractEntityEvent event, AnimalTamer oldOwner) {
-		if (chargePrice(event.getPlayer(), freePetPrice)) {
-			Tameable tameableAnimal = (Tameable) event.getRightClicked();
+	private void freePet(Player player, AnimalTamer oldOwner, Tameable tameable) {
+		if (chargePrice(player, freePetPrice)) {
 			// Free pet.
-			tameableAnimal.setTamed(false);
+			tameable.setTamed(false);
 			// Make freed pet stand up.
-			if (version >= 12 && tameableAnimal instanceof Sittable) {
-				((Sittable) tameableAnimal).setSitting(false);
-			} else if (tameableAnimal instanceof Wolf) {
-				((Wolf) tameableAnimal).setSitting(false);
-			} else if (tameableAnimal instanceof Ocelot) {
-				((Ocelot) tameableAnimal).setSitting(false);
+			if (version >= 12 && tameable instanceof Sittable) {
+				((Sittable) tameable).setSitting(false);
+			} else if (tameable instanceof Wolf) {
+				((Wolf) tameable).setSitting(false);
+			} else if (tameable instanceof Ocelot) {
+				((Ocelot) tameable).setSitting(false);
 			}
 
-			event.getPlayer().sendMessage(plugin.getChatHeader()
+			player.sendMessage(plugin.getChatHeader()
 					+ plugin.getPluginLang().getString("pet-freed", "Say goodbye: this pet returned to the wild!"));
 
 			// Create new event to allow other plugins to be aware of the freeing.
 			PlayerChangeAnimalOwnershipEvent playerChangeAnimalOwnershipEvent = new PlayerChangeAnimalOwnershipEvent(
-					oldOwner, null, tameableAnimal);
+					oldOwner, null, tameable);
 			Bukkit.getServer().getPluginManager().callEvent(playerChangeAnimalOwnershipEvent);
 		}
 	}
@@ -198,40 +206,39 @@ public class PlayerInteractListener implements Listener {
 	/**
 	 * Displays a hologram, and automatically delete it after a given delay.
 	 * 
-	 * @param event
+	 * @param player
 	 * @param owner
+	 * @param tameable
 	 */
 	@SuppressWarnings("deprecation")
-	private void displayHologramAndMessage(PlayerInteractEntityEvent event, AnimalTamer owner) {
+	private void displayHologramAndMessage(Player player, AnimalTamer owner, Tameable tameable) {
 		if (hologramMessage) {
-			Entity clickedAnimal = event.getRightClicked();
-
 			double offset = HORSE_OFFSET;
-			if (clickedAnimal instanceof Ocelot) {
-				if (!displayCat || !event.getPlayer().hasPermission("petmaster.showowner.cat")) {
+			if (tameable instanceof Ocelot) {
+				if (!displayCat || !player.hasPermission("petmaster.showowner.cat")) {
 					return;
 				}
 				offset = CAT_OFFSET;
-			} else if (clickedAnimal instanceof Wolf) {
-				if (!displayDog || !event.getPlayer().hasPermission("petmaster.showowner.dog")) {
+			} else if (tameable instanceof Wolf) {
+				if (!displayDog || !player.hasPermission("petmaster.showowner.dog")) {
 					return;
 				}
 				offset = DOG_OFFSET;
-			} else if (version >= 11 && clickedAnimal instanceof Llama) {
-				if (!displayLlama || !event.getPlayer().hasPermission("petmaster.showowner.llama")) {
+			} else if (version >= 11 && tameable instanceof Llama) {
+				if (!displayLlama || !player.hasPermission("petmaster.showowner.llama")) {
 					return;
 				}
 				offset = LLAMA_OFFSET;
-			} else if (version >= 12 && clickedAnimal instanceof Parrot) {
-				if (!displayParrot || !event.getPlayer().hasPermission("petmaster.showowner.parrot")) {
+			} else if (version >= 12 && tameable instanceof Parrot) {
+				if (!displayParrot || !player.hasPermission("petmaster.showowner.parrot")) {
 					return;
 				}
 				offset = PARROT_OFFSET;
-			} else if (!displayHorse || !event.getPlayer().hasPermission("petmaster.showowner.horse")) {
+			} else if (!displayHorse || !player.hasPermission("petmaster.showowner.horse")) {
 				return;
 			}
 
-			Location eventLocation = clickedAnimal.getLocation();
+			Location eventLocation = tameable.getLocation();
 			// Create location with offset.
 			Location hologramLocation = new Location(eventLocation.getWorld(), eventLocation.getX(),
 					eventLocation.getY() + offset, eventLocation.getZ());
@@ -254,23 +261,22 @@ public class PlayerInteractListener implements Listener {
 
 		String healthInfo = "";
 		if (showHealth) {
-			Animals damageable = (Animals) event.getRightClicked();
-			String currentHealth = String.format("%.1f", damageable.getHealth());
-			String maxHealth = version < 9 ? String.format("%.1f", damageable.getMaxHealth())
-					: String.format("%.1f", damageable.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
+			Animals animal = (Animals) tameable;
+			String currentHealth = String.format("%.1f", animal.getHealth());
+			String maxHealth = version < 9 ? String.format("%.1f", animal.getMaxHealth())
+					: String.format("%.1f", animal.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
 			healthInfo = ChatColor.GRAY + ". " + plugin.getPluginLang().getString("petmaster-health", "Health: ")
 					+ ChatColor.GOLD + currentHealth + "/" + maxHealth;
 		}
 
 		if (chatMessage) {
-			event.getPlayer().sendMessage(
-					plugin.getChatHeader() + plugin.getPluginLang().getString("petmaster-chat", "Pet owned by ")
-							+ ChatColor.GOLD + owner.getName() + healthInfo);
+			player.sendMessage(plugin.getChatHeader() + plugin.getPluginLang().getString("petmaster-chat", "Pet owned by ")
+					+ ChatColor.GOLD + owner.getName() + healthInfo);
 		}
 
 		if (actionBarMessage) {
 			try {
-				FancyMessageSender.sendActionBarMessage(event.getPlayer(), "&o" + ChatColor.GRAY
+				FancyMessageSender.sendActionBarMessage(player, "&o" + ChatColor.GRAY
 						+ plugin.getPluginLang().getString("petmaster-action-bar", "Pet owned by ") + ChatColor.GOLD
 						+ owner.getName() + healthInfo);
 			} catch (Exception e) {
