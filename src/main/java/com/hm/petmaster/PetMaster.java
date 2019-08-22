5d73e1df3af84736c05174b18eaacf4e77dda13f
PyvesB <PyvesDev@gmail.com>
2019-08-22 21:52:28 +0100

    Increased version number to 1.10.2

--- src/main/java/com/hm/petmaster/PetMaster.java
+++ src/main/java/com/hm/petmaster/PetMaster.java
@@ -38,7 +38,7 @@ import com.hm.petmaster.listener.PlayerQuitListener;
  * Spigot project page: spigotmc.org/resources/pet-master.15904
  * 
  * @since December 2015.
- * @version 1.10.1
+ * @version 1.10.2
  * @author DarkPyves
  */
 public class PetMaster extends JavaPlugin {
