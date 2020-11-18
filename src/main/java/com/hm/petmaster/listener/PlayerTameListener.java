22021f8bc55349f8f68436f64dc583a55cdc03b8
Maik Wramba <mail@mkwa.dev>
2020-11-18 20:27:05 +0100

    Check version before trying to apply color to cat entities

--- src/main/java/com/hm/petmaster/listener/PlayerTameListener.java
+++ src/main/java/com/hm/petmaster/listener/PlayerTameListener.java
@@ -1,9 +1,10 @@
 package com.hm.petmaster.listener;
 
+import com.hm.mcshared.particle.ReflectionUtils.PackageType;
 import com.hm.petmaster.PetMaster;
-import java.io.File;
 import org.bukkit.DyeColor;
 import org.bukkit.entity.Cat;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,26 +14,21 @@ import org.bukkit.event.entity.EntityTameEvent;
 public class PlayerTameListener implements Listener {
 
 	private final PetMaster plugin;
+	private final int version;
 
 	public PlayerTameListener(PetMaster plugin) {
 		this.plugin = plugin;
+		this.version = Integer.parseInt(PackageType.getServerVersion().split("_")[1]);
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
 	public void onPlayerTamePet(EntityTameEvent event) {
-		switch (event.getEntity().getType()) {
-			case CAT: {
-				DyeColor color = plugin.getSetColorCommand().getColor(event.getOwner().getUniqueId());
-				((Cat) event.getEntity()).setCollarColor(color != null ? color : DyeColor.RED);
-				break;
-			}
-			case WOLF: {
-				DyeColor color = plugin.getSetColorCommand().getColor(event.getOwner().getUniqueId());
-				((Wolf) event.getEntity()).setCollarColor(color != null ? color : DyeColor.RED);
-				break;
-			}
-			default:
-			// Nothing
+		if (version >= 14 && event.getEntity().getType().equals(EntityType.CAT)) {
+			DyeColor color = plugin.getSetColorCommand().getColor(event.getOwner().getUniqueId());
+			((Cat) event.getEntity()).setCollarColor(color);
+		} else if (event.getEntity().getType().equals(EntityType.WOLF)) {
+			DyeColor color = plugin.getSetColorCommand().getColor(event.getOwner().getUniqueId());
+			((Wolf) event.getEntity()).setCollarColor(color);
 		}
 	}
 
